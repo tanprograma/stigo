73dc061a75e0e20466c07c61924db53b60b4aee6
tanprograma <tanprograma@gmaol.com>
2024-01-11 18:17:42 +0300

    v1 -test

---
+++ cook.js
@@ -0,0 +1,3 @@
+console.log("app served as is..");
+console.log("built with love by tanprograma@stiglersgorge");
+console.log("sytem live,,, custom message");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://pharmacy2024.onrender.com/api',
 };
