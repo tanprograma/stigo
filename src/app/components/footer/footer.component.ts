081910a44c9dd85e14b275ac474bd12b75b48cc0
tanprograma <tanprograma@gmaol.com>
2024-01-25 10:29:49 +0300

    fixed footer dedix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
   @Input() created!: number;
   current = new Date().getFullYear();
-
+  dedication = '';
+  interval: any = '';
+  dedications = [
+    'made with &#10084 by PharmaBro',
+    'special thanks to Diana',
+    'much thanks to shindai',
+    'big up MO wa Lab',
+    "love ya'all",
+  ];
   constructor() {}
+  setDedication() {
+    let index = 0;
+    this.interval = setInterval(() => {
+      this.dedication = this.dedications[index];
+      if (index == this.dedications.length - 1) {
+        index = 0;
+      } else {
+        index += 1;
+      }
+    }, 3000);
+  }
 
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
