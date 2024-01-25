0e4e5d1a82e760a0052db4916c72f763aae10b25
tanprograma <tanprograma@gmaol.com>
2024-01-25 11:02:24 +0300

    fixed again footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,35 +5,11 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit, OnDestroy {
+export class FooterComponent implements OnInit {
   @Input() created!: number;
   current = new Date().getFullYear();
-  dedication = '';
-  interval: any = '';
-  dedications = [
-    'made with &#10084 by PharmaBro',
-    'special thanks to Diana',
-    'much thanks to shindai',
-    'big up MO wa Lab',
-    "love ya'all",
-  ];
-  constructor() {}
-  setDedication() {
-    let index = 0;
-    this.interval = setInterval(() => {
-      this.dedication = this.dedications[index];
-      if (index == this.dedications.length - 1) {
-        index = 0;
-      } else {
-        index += 1;
-      }
-    }, 3000);
-  }
 
-  ngOnInit(): void {
-    this.setDedication();
-  }
-  ngOnDestroy(): void {
-    clearInterval(this.interval);
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
